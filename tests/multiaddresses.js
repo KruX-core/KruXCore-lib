fd5c03d6c009cecb04ef318bf258650796339bae
TheDevMinerTV <tobigames200@gmail.com>
2019-09-14 10:40:29 +0200

    Some stuff

--- tests/multiaddresses.js
+++ tests/multiaddresses.js
@@ -5,85 +5,148 @@ const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
 // Transaction settings
-const numOfAdresses = 10;
-const numOfCoinsSentPerTx = 4;
+const numOfAdresses = 5;
+const numOfCoinsSentPerTx = 1;
 
 // Blockchain settings
-const difficulty = 1;
+const difficulty = 0;
 const numOfBlocksToMine = 100;
 
-let addresses = [];
+const numOfTestRuns = 10;
+let results = [];
 
-const Coin = new Blockchain({ verbose: true, difficulty: difficulty });
+for (let i = 0; i < numOfTestRuns; i++) {
+    let addresses = [];
 
-// Generate n keys with a public and private keypair, save them to the addresses array
-// and register them on the blockchain
-for (let i = 0; i < numOfAdresses; i++) {
-    const key = ec.genKeyPair();
-    const pubKey = key.getPublic('hex');
-    const privKey = key.getPrivate('hex');
+    const Coin = new Blockchain({
+        genesisTimestamp: 1556735351,
+        verbose: true,
+        difficulty: difficulty,
+        txFee: 0.1,
+        premineAddress: 'Genesis',
+        premineAmount: 1000
+    });
 
-    addresses.push({ key: key, pubKey: pubKey, privKey: privKey });
-    Coin.addWalletAddress(pubKey);
-}
+    console.log(`Genesis block: ${JSON.stringify(Coin.getLatestBlock(), null, 4)}`)
 
-console.log(`${numOfAdresses} addresses have been generated and registered!`);
+    // Generate n keys with a public and private keypair, save them to the addresses array
+    // and register them on the blockchain
+    for (let i = 0; i < numOfAdresses; i++) {
+        const key = ec.genKeyPair();
+        const pubKey = key.getPublic('hex');
+        const privKey = key.getPrivate('hex');
 
-// Get the start time and memory usage
-const startTime = new Date();
-let memoryUsageMiningStarted = process.memoryUsage();
-
-while (Coin.getBlockHeight() < numOfBlocksToMine) {
-    // Generate two random numbers to select two addresses for the transaction
-    const num1 = Math.floor(Math.random() * numOfAdresses);
-
-    for (let i = 0; i < addresses.length; i++) {
-        // Generate two random numbers to select two addresses for the transaction
-        const num2 = Math.floor(Math.random() * numOfAdresses);
-        const num3 = Math.floor(Math.random() * numOfAdresses);
-
-        // Check that we're not sending the transaction to ourselves
-        if (num2 !== num3) {
-            // Get the addresses from the corresponding numbers
-            let address1 = addresses[num2].pubKey;
-            let address2 = addresses[num3].pubKey;
-
-            // Check that we have enough coins on that address to send the transaction
-            if (Coin.getBalanceForAddress(address1) > numOfCoinsSentPerTx) {
-                // Create the transaction itself
-                let tx = new Transaction(Date.now(), address1, address2, numOfCoinsSentPerTx);
-
-                // Sign the transaction with the key of the sender
-                tx.signTx(addresses[num2].key);
-
-                // And finally, add the transaction to the transaction pool on the blockchain
-                Coin.addTransaction(tx);
-            }
-        }
+        addresses.push({ key: key, pubKey: pubKey, privKey: privKey });
+        Coin.addWalletAddress(pubKey);
     }
 
-    Coin.mineCurrentBlock(addresses[num1].pubKey);
+    // console.log(`${numOfAdresses} addresses have been generated and registered!`);
+
+    // Get the start time and memory usage
+    const startTime = new Date();
+    let memoryUsageMiningStarted = process.memoryUsage();
+
+    while (Coin.getBlockHeight() < numOfBlocksToMine) {
+        // Generate two random numbers to select two addresses for the transaction
+        const num1 = Math.floor(Math.random() * numOfAdresses);
+
+        for (let i = 0; i < addresses.length; i++) {
+            // Generate two random numbers to select two addresses for the transaction
+            const num2 = Math.floor(Math.random() * numOfAdresses);
+            const num3 = Math.floor(Math.random() * numOfAdresses);
+
+            // Check that we're not sending the transaction to ourselves
+            if (num2 !== num3) {
+                // Get the addresses from the corresponding numbers
+                let address1 = addresses[num2].pubKey;
+                let address2 = addresses[num3].pubKey;
+
+                console.log(`Balance of ${address1}: ${Coin.getBalanceForAddress(address1)}`)
+                // Check that we have enough coins on that address to send the transaction
+                if (Coin.getBalanceForAddress(address1) > numOfCoinsSentPerTx) {
+                    // Create the transaction itself
+                    let tx = new Transaction(Date.now(), address1, address2, numOfCoinsSentPerTx, 0.1);
+
+                    // Sign the transaction with the key of the sender
+                    tx.signTx(addresses[num2].key);
+
+                    // And finally, add the transaction to the transaction pool on the blockchain
+                    Coin.addTransaction(tx);
+                }
+            }
+        }
+
+        Coin.mineCurrentBlock(addresses[num1].pubKey);
+    }
+
+    // Get time and memory usage after mining
+    const validateStartTime = new Date();
+    let memoryUsageValidationStarted = process.memoryUsage();
+
+    // Check the blockchain for fuckery
+    console.log(`\nIs blockchain valid? ${Coin.isChainValid()}`);
+
+    // Get time and memory usage after validating
+
+    const validateFinishedTime = new Date();
+    let memoryUsageValidationFinished = process.memoryUsage();
+
+    results.push({
+        timeSpentMining: validateStartTime - startTime,
+        timeSpentMiningPerBlock: (validateStartTime - startTime) / Coin.getBlockHeight(),
+        timeSpentValidating: validateFinishedTime - validateStartTime,
+        timeSpentValidatingPerBlock: (validateFinishedTime - validateStartTime) / Coin.getBlockHeight(),
+        memoryBeforeMining: memoryUsageMiningStarted.rss / 1024 / 1024,
+        memoryBeforeValidating: memoryUsageValidationStarted.rss / 1024 / 1024,
+        memoryAfterValidating: memoryUsageValidationFinished.rss / 1024 / 1024
+    });
 }
 
-// Get time and memory usage after mining
-const validateStartTime = new Date();
-let memoryUsageValidationStarted = process.memoryUsage();
+const result = (()=> {
+    let _tmp = {
+        timeSpentMining: 0,
+        timeSpentMiningPerBlock: 0,
+        timeSpentValidating: 0,
+        timeSpentValidatingPerBlock: 0,
 
-// Check the blockchain for fuckery
-console.log(`\nIs blockchain valid? ${Coin.isChainValid()}`);
+        memoryBeforeMining: 0,
+        memoryBeforeValidating: 0,
+        memoryAfterValidating: 0
+    };
 
-// Get time and memory usage after validating
+    results.forEach(result => {
+        _tmp.timeSpentMining += result.timeSpentMining;
+        _tmp.timeSpentMiningPerBlock += result.timeSpentMiningPerBlock;
+        _tmp.timeSpentValidating += result.timeSpentValidating;
+        _tmp.timeSpentValidatingPerBlock += result.timeSpentValidatingPerBlock;
 
-const validateFinishedTime = new Date();
-let memoryUsageValidationFinished = process.memoryUsage();
+        _tmp.memoryBeforeMining += result.memoryBeforeMining;
+        _tmp.memoryBeforeValidating += result.memoryBeforeValidating;
+        _tmp.memoryAfterValidating += result.memoryAfterValidating;
+    });
 
-// Print out some debug info
+    console.log(_tmp)
+
+    _tmp = {
+        timeSpentMining: _tmp.timeSpentMining / numOfTestRuns,
+        timeSpentMiningPerBlock: _tmp.timeSpentMiningPerBlock / numOfTestRuns,
+        timeSpentValidating: _tmp.timeSpentValidating / numOfTestRuns,
+        timeSpentValidatingPerBlock: _tmp.timeSpentValidatingPerBlock / numOfTestRuns,
+
+        memoryBeforeMining: _tmp.memoryBeforeMining / numOfTestRuns,
+        memoryBeforeValidating: _tmp.memoryBeforeValidating / numOfTestRuns,
+        memoryAfterValidating: _tmp.memoryAfterValidating / numOfTestRuns
+    };
+
+    return _tmp
+})()
+
 console.log('\nCopy paste me\n------------------------------------');
-console.log(`\nDifficulty ${difficulty}`);
-console.log(`Mining took ${validateStartTime - startTime} milliseconds`);
-console.log(`Mining time per block is ${(validateStartTime - startTime) / Coin.getBlockHeight()} milliseconds`);
-console.log(`Validating took ${validateFinishedTime - validateStartTime} milliseconds`);
-console.log(`Validation time per block is ${(validateFinishedTime - validateStartTime) / Coin.getBlockHeight()} milliseconds`);
-console.log(`Memory usage before mining: ${memoryUsageMiningStarted.rss / 1024 / 1024} MB`);
-console.log(`Memory usage before validating: ${memoryUsageValidationStarted.rss / 1024 / 1024} MB`);
-console.log(`Memory usage after validating: ${memoryUsageValidationFinished.rss / 1024 / 1024} MB`);
+console.log(`\n- difficulty: ${difficulty}`);
+console.log(`- time_spent_mining: ${result.timeSpentMining.toFixed(4)}`);
+console.log(`  time_spent_mining_per_block: ${result.timeSpentMiningPerBlock.toFixed(4)}`);
+console.log(`  time_spent_validating: ${result.timeSpentValidating.toFixed(4)}`);
+console.log(`  time_spent_validating_per_block: ${result.timeSpentValidatingPerBlock.toFixed(4)}`);
+console.log(`  memory_before_mining: ${result.memoryBeforeMining.toFixed(4)}`);
+console.log(`  memory_before_validating: ${result.memoryBeforeValidating.toFixed(4)}`);
+console.log(`  memory_after_validating: ${result.memoryAfterValidating.toFixed(4)}`);
--- tests/simpleaddresses.js
+++ tests/simpleaddresses.js
@@ -6,22 +6,27 @@ const ec = new EC('secp256k1');
 
 const key1 = ec.keyFromPrivate('3236f563f41037771bc70d0cdd13b8f823c371cea08dd55ab6f8ace221d33718');
 const key2 = ec.keyFromPrivate('be3c0c3143be5594b47a09ba12c805aaaecc1ee003143376dbb7017d231316ac');
-const key3 = ec.keyFromPrivate('abe26910fb34a71150100aede4fd1707c01e0abdefec83f242a95ed61004173d');
 
 const walletAddr1 = key1.getPublic('hex');
 const walletAddr2 = key2.getPublic('hex');
-const walletAddr3 = key3.getPublic('hex');
 
-const verbose = true;
-const difficulty = 3;
+const verbose = false;
+const difficulty = 1;
+const blocksToMine = 15;
 
 // Instanciate a new Blockchain
-let Coin = new Blockchain({ verbose: verbose, difficulty: difficulty });
+const Coin = new Blockchain({
+    genesisTimestamp: 1556735351,
+    verbose: true,
+    difficulty: difficulty,
+    txFee: 0.1,
+    premineAddress: 'Genesis',
+    premineAmount: 1000
+});
 
 // Add our wallets as a registered address
 Coin.addWalletAddress(walletAddr1);
 Coin.addWalletAddress(walletAddr2);
-Coin.addWalletAddress(walletAddr3);
 Coin.addWalletAddress(walletAddr2);
 
 const startTime = new Date();
@@ -30,7 +35,6 @@ const startTime = new Date();
 let tx1 = new Transaction(Date.now(), walletAddr1, walletAddr2, 1);
 tx1.signTx(key1);
 console.log('Transaction successfully signed!');
-console.log(`Block reward: ${Coin.blockReward}`);
 
 let memoryUsageMiningStarted = process.memoryUsage();
 
@@ -41,14 +45,13 @@ Coin.mineCurrentBlock(walletAddr1);
 // Add the transaction to the pending transactions
 Coin.addTransaction(tx1);
 
-while (Coin.getBlockHeight() < 2500) { // mine 2.5k blocks
+while (Coin.getBlockHeight() < blocksToMine) { // mine 2.5k blocks
     Coin.mineCurrentBlock(walletAddr1);
 }
 
 let memoryUsageMiningFinished = process.memoryUsage();
 
 const validateStartTime = new Date();
-let memoryUsageValidationStarted = process.memoryUsage();
 
 console.log(`\nIs blockchain valid? ${Coin.isChainValid()}`);
 
@@ -56,12 +59,12 @@ const validateFinishedTime = new Date();
 let memoryUsageValidationFinished = process.memoryUsage();
 
 console.log('\nCopy paste me\n------------------------------------');
-console.log(`\nDifficulty ${difficulty}`);
+console.log(`\n## Difficulty ${difficulty}\n`);
+console.log(`${blocksToMine} blocks mined`);
 console.log(`Mining took ${validateStartTime - startTime} milliseconds`);
 console.log(`Mining time per block is ${(validateStartTime - startTime) / Coin.getBlockHeight()} milliseconds`);
 console.log(`Validating took ${validateFinishedTime - validateStartTime} milliseconds`);
 console.log(`Validation time per block is ${(validateFinishedTime - validateStartTime) / Coin.getBlockHeight()} milliseconds`);
 console.log(`Memory usage before mining: ${memoryUsageMiningStarted.rss / 1024 / 1024} MB`);
 console.log(`Memory usage after mining: ${memoryUsageMiningFinished.rss / 1024 / 1024} MB`);
-console.log(`Memory usage before validating: ${memoryUsageValidationStarted.rss / 1024 / 1024} MB`);
 console.log(`Memory usage after validating: ${memoryUsageValidationFinished.rss / 1024 / 1024} MB`);
